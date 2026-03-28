c8fd88592284d789eb6a18c6ba359fae93eab41d
Michael Begisen <michaelmshindi@gmail.com>
2026-03-28 20:45:58 +0300

    fix this

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Mbaya&KanyoroLLP/", // 👈 ADD THIS LINE
+  base: "/mbaya-kanyoro/", // 👈 ADD THIS LINE
 
   server: {
     host: "::",
